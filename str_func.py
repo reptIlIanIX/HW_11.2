388492933dd788b622cbfe223eb1c617406a6dd3
reptilianIX <reptlianIX@gmail.com>
2023-05-01 20:13:52 +0300

    added second f in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,5 +1,7 @@
 def upper_letter(letters):
+    """буквы становится ЗАГЛАВНЫМИ"""
     return letters.upper()
 
 def capital_letter_f(letters):
+    """каждое слово начинается с Большой Буквы"""
     return letters.title()
